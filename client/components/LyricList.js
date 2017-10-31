da43a47508d617783e49dd5d273e26b58abb264e
Matthew Holman <mateoholman@gmail.com>
2017-10-31 14:39:44 -0500

    Added likes to each lyric

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
   onLike(id) {
-    console.log('Liked!');
+    this.props.mutate({
+      variables: { id }
+    });
   }
 
   renderList() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className="collection-item">
           {content}
           <i className="material-icons" onClick={() => this.onLike(id)}>
             thumb_up
           </i>
+          {likes}
         </li>
       );
     });
@@ -22,4 +27,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
